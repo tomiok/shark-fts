d70c1187a9c3de96235f69bfc4dd640a4c15cce6
tomas lingotti <tomaslingotti@gmail.com>
2021-01-13 22:36:44 -0300

    WIP

---
+++ analyzer.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"strings"
+	"unicode"
+)
+
+var stopWords = map[string]struct{}{
+	"a": {}, "and": {}, "am":{}, "be": {}, "have": {}, "i": {},
+	"in": {}, "of": {}, "that": {}, "the": {}, "to": {},
+}
+
+func analyze(text string) []string{
+	tokens := tokenizer(text)
+	tokens = toLowerCase(tokens)
+	tokens = stopWordsFilter(tokens)
+	return tokens
+}
+
+// tokenizer
+// hola como estas?
+// [hola, como, estas]
+func tokenizer(text string) []string {
+	return strings.FieldsFunc(text, func(r rune) bool {
+		return !unicode.IsLetter(r) && !unicode.IsNumber(r)
+	})
+}
+
+				// filters
+
+//toLowerCase
+func toLowerCase(tokens []string) []string {
+
+	r := make([]string, len(tokens))
+
+	for i, t := range tokens {
+		r[i] = strings.ToLower(t)
+	}
+	return r
+}
+
+//stopWordsFilter
+func stopWordsFilter(tokens []string) []string {
+	r := make([]string, 0, len(tokens))
+
+	for _, token := range tokens {
+		if _, ok := stopWords[token]; !ok {
+			r = append(r, token)
+		}
+	}
+	return r
+}
